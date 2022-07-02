723e620350144868cae4d5507c13244690ca0a72
Dayana Betancur <dahianab01@gmail.com>
2022-07-01 19:13:14 -0500

    JS complete

--- include/js/formulario.js
+++ include/js/formulario.js
@@ -3,6 +3,7 @@ var error = document.getElementById('error');
 var inputCorreo = document.getElementById('correo');
 var errorCorreo =document.getElementById('errorCorreo');
 
+
 inputNombre.addEventListener('input', function(){
     if(this.value.length > 20){
         this.value = this.value.slice(0,20);
@@ -15,11 +16,12 @@ inputNombre.addEventListener('input', function(){
 });
 
 inputCorreo.addEventListener('input', function(){
-    var campo = EventTarget;       
+    campo = document.getElementById('correo');       
     inputCorreoRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
     if (inputCorreoRegex.test(campo.value)) {
-      errorCorreo.hidden=false;
-    } else {
       errorCorreo.hidden=true;
+    } else {
+      errorCorreo.hidden=false;
     }
 });
+
