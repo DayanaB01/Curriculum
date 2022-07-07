6a9f704e99cec65d964e237a006d93fb1d8d9566
Dayana Betancur <dahianab01@gmail.com>
2022-07-06 19:43:26 -0500

    new function

--- include/js/formulario.js
+++ include/js/formulario.js
@@ -2,13 +2,13 @@ var inputNombre = document.getElementById('nombre');
 var error = document.getElementById('error');
 var inputCorreo = document.getElementById('correo');
 var errorCorreo =document.getElementById('errorCorreo');
-
+var area = document.getElementById('area');
+var errorMessage = document.getElementById('errorMessage');
+var btnSent = document.getElementById('confirmar');
 
 inputNombre.addEventListener('input', function(){
-    if(this.value.length > 20){
-        this.value = this.value.slice(0,20);
-    } 
-    if (this.value.length < 3){
+    if(inputNombre.value.length > 20 || inputNombre.value.length < 3){
+        inputNombre.value = inputNombre.value.slice(0,20);
         error.hidden= false;
     }else{
         error.hidden= true;
@@ -25,3 +25,16 @@ inputCorreo.addEventListener('input', function(){
     }
 });
 
+area.addEventListener('input', function(){
+    if(area.value.length < 2){
+        errorMessage.hidden = false;
+    }else{
+        errorMessage.hidden = true;
+    }
+});
+
+btnSent.addEventListener('submit', e=>{
+    if(error.hidden === false || errorCorreo === false|| errorMessage === false){
+        e.preventDefault();
+    }
+});
